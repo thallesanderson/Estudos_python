f0da5f2d670c4a7f1dcaea77afcca18a52bbc5cb
Thalles Anderson <77176078+thallesanderson@users.noreply.github.com>
2024-08-21 06:51:45 -0300

    estudos em python

---
+++ Aula01.py
@@ -0,0 +1,66 @@
+# Tipos de dados
+# print('BOLEANO:',type(False),'STRING:', type('Thalles'),'INTEIRO:',type(22),'FLOAT:',type(1.2))
+# print(int('2'),type(int('3')))
+# print(int('2')+ 3)
+# primeira_nota = float(input('Informe a Primeira nota: '))
+# segunda_nota = float(input('Informe a Segunda nota: '))
+# soma = primeira_nota + segunda_nota
+# nome_user = input('Informe seu nome: ')
+# print(nome_user,'Sua Nota é:', soma)
+
+# Solicita ao usuário que insira os dois números
+# numero1 = float(input("Digite o primeiro número: "))
+# numero2 = float(input("Digite o segundo número: "))
+
+# # Calcula a soma
+# soma = numero1 + numero2
+
+# # Exibe o resultado em um popup (utilizando o módulo tkinter para criar a interface gráfica)
+# import tkinter as tk
+# from tkinter import messagebox
+
+# # Cria a janela principal
+# root = tk.Tk()
+# root.withdraw()  # Esconde a janela principal
+
+# # Mostra a mensagem com o resultado
+# messagebox.showinfo("Resultado", f"A soma de {numero1} e {numero2} é {soma}")
+
+# # Finaliza a aplicação
+# root.destroy()
+
+# nome = str(input("Informe seu nome: "))
+# sobrenome = str(input("Informe seu sobrenome: "))
+# idade = int(input("Informe sua idade: "))
+# ano_de_nascimento = int(input("Informe seu ano de nascimento: "))
+# maior_de_idade =  idade >= 18
+# altura_metros = float(input("Informe a sua altura: "))
+# print("------------------------------")
+# print("nome: ",nome)
+# print("Sobrenome: ",sobrenome)
+# print("idade: ",idade)
+# print("ano de nascimento: ",ano_de_nascimento)
+# print("Você é maior de idade: ",maior_de_idade)
+# print("altura:",altura_metros)
+# print("------------------------------")
+
+# cont_1 = (1 + 1) ** (5 + 5)
+# print(cont_1)
+
+#  CALCULA IMC COM DUAS CASAS DECIMAIS APÓS A VIRGULA
+# nome = str(input('Seu Nome: '))
+# altura = float(input('Sua Altura: '))
+# peso = float(input('Seu Peso: '))
+# imc = float(peso / (altura * altura))
+# resultado = f'{nome} Seu Peso é {peso:.2f} Sua Altura é {altura:.2f}  Seu IMC é {imc:.2f}'
+# print(resultado)
+# print(Nome ,'Seu peso é ', '{:.2f}'.format(peso) ,'Sua altura é: ', '{:.2f}'.format(altura) , 'Seu IMC é: ', '{:.2f}'.format(imc))
+
+# A= "Thalles"
+# B= "Anderson"
+# c= 3.3
+# string = 'A={dado1} B={dado2} C={dado3:.2f}'
+# formato = string.format(dado1=A,dado2=B,dado3=c)
+# print(formato)
+
+
